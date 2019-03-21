72905fdacc51a8a844bd88e2dacafc51f3b80f6e
Tarrke <tarrke@gmail.com>
2019-03-21 09:26:44 +0100

    make chat a component

---
+++ client/src/Components/Chat.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react'
+
+export default class Chat extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            data: [],
+            input_value: "",
+        }
+
+        this.handleChat = this.handleChat.bind(this);
+        this.callChatApi = this.callChatApi.bind(this);
+        this.handleChatInputChange = this.handleChatInputChange.bind(this);
+    }
+
+    componentDidMount() {
+        setInterval(this.handleChat, 5000);
+    }
+
+    handleSubmit(event) {
+        console.log("Chat text submit: ", event);
+        this.handleChat()
+    }
+
+    handleChatInputChange(event) {
+        this.setState({ input_value: event.target.value });
+    }
+
+    handleChat() {
+        this.callChatApi()
+            .then(res => this.setState({ data: res.data }))
+            .catch(err => console.log(err));
+    }
+
+    callChatApi = async () => {
+        const response = await fetch('/chat_data');
+        const body = await response.json();
+
+        if (response.status !== 200) {
+            throw Error(body.message)
+        }
+        return body;
+    }
+
+    render() {
+        let output = this.state.data.slice(0).reverse().map((message, index) => {
+            return (<li key={index}>{message.user}: {message.message}</li>);
+        });
+        return (
+            <div id='chat-tab' className='ChatTab'>
+                <div className='ChatSection'>
+                    <ul>
+                        {output}
+                    </ul>
+                </div>
+                <div className='ChatForm'>
+                    <form onSubmit={this.handleSubmit}>
+                        <input type='text' value={this.state.input_value} onChange={this.handleChatInputChange} />
+                        <input type='submit' value='Send' />
+                    </form>
+                </div>
+            </div>
+        );
+    }
+}
